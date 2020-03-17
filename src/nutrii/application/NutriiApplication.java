077736ee6b5a8fb166c5d2a6f0f58b0e9bfd1d02
EllairaT <EllairaT@users.noreply.github.com>
2020-03-17 18:27:16 +1300

    diff main and newbranch

--- src/nutrii/application/NutriiApplication.java
+++ src/nutrii/application/NutriiApplication.java
@@ -11,7 +11,6 @@ public class NutriiApplication {
      */
     public static void main(String[] args) {
         System.out.println("thing");
-        System.out.println("newthing");
     }
     
 }
