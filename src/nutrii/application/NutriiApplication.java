31afeb2a0efe17665a0f2111124cdcd1895553a0
EllairaT <EllairaT@users.noreply.github.com>
2020-03-17 18:30:08 +1300

    test commit

--- src/nutrii/application/NutriiApplication.java
+++ src/nutrii/application/NutriiApplication.java
@@ -11,6 +11,7 @@ public class NutriiApplication {
      */
     public static void main(String[] args) {
         System.out.println("thing");
+        System.out.println("newthing2");
     }
     
 }
