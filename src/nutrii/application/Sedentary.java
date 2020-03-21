ab623f3d7eea29933d5455bd79855678c0db65de
EllairaT <EllairaT@users.noreply.github.com>
2020-03-21 13:58:07 +1300

    demo

--- src/nutrii/application/Sedentary.java
+++ src/nutrii/application/Sedentary.java
@@ -11,6 +11,7 @@ public class Sedentary extends User{
     public Sedentary(String n, char g, LocalDate d, float h, float w){
        super(n,g,d,h,w);
        modifier = 1.2f;
-       
+       //oh look a new line!
+        System.out.println("sdajfkhds");
     }
 }
