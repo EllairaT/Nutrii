70a6491fbf2031f4230d6c66291b223c32ce8610
EllairaT <EllairaT@users.noreply.github.com>
2020-04-19 20:52:10 +1200

    created method to read user file when program is run

--- src/nutrii/application/ModeratelyActive.java
+++ src/nutrii/application/ModeratelyActive.java
@@ -7,10 +7,15 @@ import java.time.LocalDate;
  * @author Ellaira
  */
 public class ModeratelyActive extends User {
-
+    
     public ModeratelyActive(String n, char g, LocalDate d, float h, float w) {
-       super(n,g,d,h,w);
-       modifier = 1.55f;
+        super(n, g, d, h, w);
+        modifier = 1.55f;
+    }
+
+    public ModeratelyActive(String s) {
+        super(s);
+        modifier = 1.55f;
     }
 
 }
--- src/nutrii/application/Sedentary.java
+++ src/nutrii/application/Sedentary.java
@@ -7,9 +7,13 @@ import java.time.LocalDate;
  * @author Ellaira
  */
 public class Sedentary extends User{
-
+    //float modifier;
     public Sedentary(String n, char g, LocalDate d, float h, float w){
        super(n,g,d,h,w);
        modifier = 1.2f;
+    }
+    public Sedentary(String s){
+        super(s);
+        modifier = 1.2f;
     }
 }
